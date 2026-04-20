6809208d67f313373f9c05832f49f1e338bddd28
Darshh-tech <bothradarsh06@gmail.com>
2026-04-20 22:10:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Task1 from './Components/task1';
+import Task2 from './Components/task2';
+import Task3 from './Components/task3';
+import Task4 from './Components/task4';
+import Task5 from './Components/task5';
+import Task6 from './Components/task6';
+
+function App() {
+  return (
+    <div className="app-container">
+
+      <Task1 />
+      <Task2 />
+      <Task3 />
+      <Task4 />
+      <Task5 />
+      <Task6 />
+    </div>
+  );
+}
+
+export default App;
